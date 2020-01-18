4700efe3059e8254b36de41d6e2c8cc5a1422583
Jérôme Wacongne <ch4mp@c4-soft.com>
2020-01-18 21:12:19 +0000

    fix proxy.conf.js (CORS not configured on the server)

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,8 +1,11 @@
 const PROXY_CONFIG = [{
-  "context": ["/api.html", "/solutions"],
-  "target": "http://localhost:8080",
-  "secure": false,
-  "changeOrigin": true
+  context: [
+    "/api.html",
+    "/solutions"
+  ],
+  target: "http://localhost:8080",
+  secure: false,
+  changeOrigin: false
 }];
 
 module.exports = PROXY_CONFIG;
