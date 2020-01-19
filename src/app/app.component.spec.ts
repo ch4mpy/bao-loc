e4ba40783efbb687e563a7f87e372c7b73abe9a0
Jérôme Wacongne <ch4mp@c4-soft.com>
2020-01-19 02:01:06 +0000

    test AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,11 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let rootElement: HTMLElement;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -14,18 +17,20 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  beforeEach(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    component = fixture.componentInstance;
+    rootElement = fixture.nativeElement as HTMLElement;
+    fixture.detectChanges();
   });
 
-  it(`should render a toolbar with a title and home icon'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelectorAll('.mat-toolbar')).toBeTruthy();
-    expect(compiled.querySelectorAll('.mat-toolbar .header-title')).toBeTruthy();
-    expect(compiled.querySelectorAll('.mat-toolbar .mat-icon')).toBeTruthy();
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render a toolbar with a title and home icon', () => {
+    expect(rootElement.querySelectorAll('mat-toolbar').length).toEqual(1);
+    expect(rootElement.querySelector('mat-toolbar .header-title').textContent).toBeTruthy();
+    expect(rootElement.querySelectorAll('mat-toolbar mat-icon').length).toEqual(1);
   });
 });
