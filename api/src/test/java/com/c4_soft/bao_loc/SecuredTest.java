c45634547a072c01bca40c80730e1e0a1cb182c4
ch4mpy <ch4mp@c4-soft.com>
2022-08-17 21:51:40 -1000

    minor versions bump

--- api/src/test/java/com/c4_soft/bao_loc/SecuredTest.java
+++ api/src/test/java/com/c4_soft/bao_loc/SecuredTest.java
@@ -5,24 +5,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
-import com.c4_soft.bao_loc.SolutionsApi.WebSecurityConfig;
-import com.c4_soft.springaddons.security.oauth2.config.synchronised.ServletSecurityBeans;
-import com.c4_soft.springaddons.security.oauth2.test.mockmvc.AddonsWebmvcTestConf;
-import com.c4_soft.springaddons.security.oauth2.test.mockmvc.AutoConfigureSecurityAddons;
+import com.c4_soft.springaddons.security.oauth2.test.mockmvc.jwt.AutoConfigureAddonsSecurityWebmvcJwt;
 
 /**
  * Avoid MethodArgumentConversionNotSupportedException with repos MockBean
  *
  * @author Jérôme Wacongne &lt;ch4mp#64;c4-soft.com&gt;
  */
-@Import(WebSecurityConfig.class)
-@AutoConfigureSecurityAddons
+@AutoConfigureAddonsSecurityWebmvcJwt
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@ImportAutoConfiguration(classes = { AddonsWebmvcTestConf.class, ServletSecurityBeans.class })
+@Import({ SolutionsApi.WebSecurityConfig.class })
 public @interface SecuredTest {
 
 }
