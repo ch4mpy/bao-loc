149f7c7a135245a0ab1829a3d507eebfc4fa9ac3
ch4mpy <ch4mp@c4-soft.com>
2023-06-14 16:08:13 -1000

    npm update

--- api/gateway/src/main/java/com/c4soft/springaddons/samples/bff/gateway/GatewayController.java
+++ api/gateway/src/main/java/com/c4soft/springaddons/samples/bff/gateway/GatewayController.java
@@ -2,14 +2,11 @@ package com.c4soft.springaddons.samples.bff.gateway;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
@@ -21,7 +18,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 
-import com.c4_soft.springaddons.security.oauth2.OpenidClaimSet;
 import com.c4_soft.springaddons.security.oauth2.config.LogoutRequestUriBuilder;
 import com.c4_soft.springaddons.security.oauth2.config.SpringAddonsOAuth2ClientProperties;
 
@@ -29,7 +25,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -61,24 +56,6 @@ public class GatewayController {
 		return Mono.just(isAuthenticated ? List.of() : this.loginOptions);
 	}
 
-	@GetMapping(path = "/me", produces = "application/json")
-	@Tag(name = "getMe")
-	@Operation(responses = { @ApiResponse(responseCode = "200") })
-	public Mono<UserDto> getMe(Authentication auth) {
-		if (auth instanceof OAuth2AuthenticationToken oauth && oauth.getPrincipal() instanceof OidcUser user) {
-			final var claims = new OpenidClaimSet(user.getClaims());
-			return Mono.just(
-					new UserDto(
-							Optional.ofNullable(claims.getIssuer()).map(URL::toString).orElse(""),
-							claims.getSubject(),
-							claims.getPreferredUsername(),
-							claims.getEmail(),
-							claims.getPicture(),
-							oauth.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList()));
-		}
-		return Mono.just(UserDto.ANONYMOUS);
-	}
-
 	@PutMapping(path = "/logout", produces = "application/json")
 	@Tag(name = "logout")
 	@Operation(responses = { @ApiResponse(responseCode = "204") })
@@ -98,10 +75,6 @@ public class GatewayController {
 		}).map(logoutUri -> {
 			return ResponseEntity.noContent().location(logoutUri).build();
 		});
-	}
-
-	static record UserDto(@NotNull String issuer, @NotNull String subject, String displayName, String email, String picture, List<String> roles) {
-		static final UserDto ANONYMOUS = new UserDto("", "", "", "", "", List.of());
 	}
 
 	static record LoginOptionDto(@NotEmpty String label, @NotEmpty String loginUri) {
